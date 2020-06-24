82110a44e42ab39d8d4bd49f869d76dbdb1cb995
Jiri Spac <capajj@gmail.com>
2020-06-24 11:37:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-'use strict';
-let { useState, useEffect } = require('react');
+import { useState, useEffect } from 'react'
 
 function getSize() {
   return {
     innerHeight: window.innerHeight,
     innerWidth: window.innerWidth,
     outerHeight: window.outerHeight,
-    outerWidth: window.outerWidth,
-  };
+    outerWidth: window.outerWidth
+  }
 }
 
-function useWindowSize() {
-  let [windowSize, setWindowSize] = useState(getSize());
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState(getSize())
 
   function handleResize() {
-    setWindowSize(getSize());
+    setWindowSize(getSize())
   }
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize)
     return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
-  return windowSize;
+  return windowSize
 }
-
-module.exports = useWindowSize;
